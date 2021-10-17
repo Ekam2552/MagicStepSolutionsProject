be37cd8a9918b26da7f48938fcf9c1da5002fc05
Ekampreet Singh <ekam2552@gmail.com>
2021-10-17 17:01:51 +0530

    Further edited Other Orders page

--- src/Pages/OtherOrdersPage/OtherOrdersPage.js
+++ src/Pages/OtherOrdersPage/OtherOrdersPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./OtherOrdersPage.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt, faTruckMoving } from '@fortawesome/free-solid-svg-icons';
+
 export const OtherOrdersPage = () => {
     return(
         <div className="OtherOrdersPageContainer">
@@ -22,9 +25,36 @@ export const OtherOrdersPage = () => {
                 <div className="OtherOrdersPageLowerMiddle"></div>
                 <div className="OtherOrdersPageLowerRight">
                     <div className="OtherOrdersPageBuyerInformation">
-                        <div className="OtherOrdersPageOrderTypeDiv"></div>
+                        <div className="OtherOrdersPageOrderTypeDiv">
+                            <div className="OtherOrdersPageOrderTypeHeadingDiv">Order Type</div>
+                            <div className="OtherOrdersPageOrderTypeInformationDiv">
+                                <div className="OtherOrdersPageOrderTypeIconDiv">
+                                    <FontAwesomeIcon icon={faTruckMoving} />
+                                </div>
+                                <div className="OtherOrdersPageOrderTypeText">Home Delivery</div>
+                            </div>
+                        </div>
                         <div className="OtherOrdersPageBuyerInformationDividingLine"></div>
-                        <div className="OtherOrdersPageBuyerAddressDiv"></div>
+                        <div className="OtherOrdersPageBuyerAddressDiv">
+                            <div className="OtherOrdersPageBuyerAddressInformationDiv">
+                                <div className="OtherOrdersPageBuyerAddressMapMarkerIconDiv">
+                                    <FontAwesomeIcon icon={faMapMarkerAlt} />
+                                </div>
+                                <div className="OtherOrdersPageBuyerAddressTextDiv">
+                                    <div className="OtherOrdersPageBuyerAddressHeading">Delivery Address</div>
+                                    <div className="OtherOrdersPageBuyerAddresseeName">Mriganka Baruah</div>
+                                    <div className="OtherOrdersPageBuyerAddressLines">House No 15</div>
+                                    <div className="OtherOrdersPageBuyerAddressLines">Near Greenland</div>
+                                    <div className="OtherOrdersPageBuyerAddressLines">North lakhimpur</div>
+                                    <div className="OtherOrdersPageBuyerAddressLines">Assam</div>
+                                    <div className="OtherOrdersPageBuyerAddressLines">India</div>
+                                    <div className="OtherOrdersPageBuyerAddresseePhoneNumber">+911234567890</div>
+                                </div>
+                            </div>
+                            <div className="OtherOrdersPageBuyerAddressDetailsButtonDiv">
+                                <button className="OtherOrdersPageBuyerAddressDetailsButton">Details</button>
+                            </div>
+                        </div>
                         <div className="OtherOrdersPageBuyerInformationDividingLine"></div>
                         <div className="OtherOrdersPagePaymentMethod"></div>
                     </div>
