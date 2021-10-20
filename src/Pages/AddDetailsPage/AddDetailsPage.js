74a03a7d5b04356876ef30da4f1daada31fc9872
Ekampreet Singh <ekam2552@gmail.com>
2021-10-21 04:24:38 +0530

    Completely remade responsive Add Details page.

--- src/Pages/AddDetailsPage/AddDetailsPage.js
+++ src/Pages/AddDetailsPage/AddDetailsPage.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import "./AddDetailsPage.css";
 
-import { TextAreaWithLabel } from "../../Components/TextAreaWithLabelComponent/TextAreaWithLabelComponent";
-
 import { Button as ReactButton } from "react-bootstrap";
 
 export const AddDetailsPage = () => {
     return(
         <div className="AddDetailsPageContainer">
             <div className="AddDetailsPageTitleDiv">Add Details</div>
-            <div className="AddDetailsPageAboutDiv"><TextAreaWithLabel labelText="About" TextAreaHeight="6" placeholderText="Help users to know more about your business" /></div>
-            <div className="AddDetailsPageReturnPolicyDiv"><TextAreaWithLabel labelText="Return Policy" TextAreaHeight="8" placeholderText="Mention about your return policy in details" /></div>
+            <div className="AddDetailsPageAboutDiv">
+                <div className="AddDetailsPageAboutHeadingDiv">About</div>
+                <div className="AddDetailsPageAboutTextAreaDiv">
+                    <textarea className="AddDetailsPageAboutTextArea" placeholder="Help users to know more about your business" />
+                </div>
+            </div>
+            <div className="AddDetailsPageReturnPolicyDiv">
+                <div className="AddDetailsPageReturnPolicyHeadingDiv">Return Policy</div>
+                <div className="AddDetailsPageReturnPolicyTextAreaDiv">
+                    <textarea className="AddDetailsPageReturnPolicyTextArea" placeholder="Mention about your return policy in details" />
+                </div>
+            </div>
             <div className="AddDetailsPageSubmitButtonDiv">
                 <ReactButton variant="success" className="AddDetailsPageSubmitButton">Submit</ReactButton>
             </div>
