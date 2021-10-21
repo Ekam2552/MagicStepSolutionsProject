1cb582119a74915dc4ab5d54ec9c8a1296363730
Ekampreet Singh <ekam2552@gmail.com>
2021-10-21 05:35:24 +0530

    Made BookKeeping page responsive

--- src/Components/CardComponent/CardComponent.js
+++ src/Components/CardComponent/CardComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./CardComponent.css";
 
-export const CardComponent = ({ imageUrl, title, note }) => {
+export const CardComponent = ({ imageUrl, title, note, widthGiven, heighGiven }) => {
     return(
-        <div className="CardContainer">
+        <div className="CardContainer" style={{ width: `${widthGiven}`, height: `${heighGiven}` }}>
             <div className="imageContainer">
                 <img src={imageUrl} alt={title} />
             </div>
